dc7e4b733fcd2e4146f614776e258bcfbfbd2e3d
Baerke <79163400+BaerkeDestroyer@users.noreply.github.com>
2022-01-16 10:52:23 +0100

    Update about.js

--- src/commands/information/about.js
+++ src/commands/information/about.js
@@ -12,7 +12,7 @@ module.exports = {
 			.addFields(
 				{ name: 'Use', value: `This bot is our all in one super discord bot.`},
 				{ name: 'Development', value: `This bot is mainly developed by <@!405176812684181514> and simple commands are done by me <@!521743464061468672>`},
-                { name: 'Repo', value: `This bot is open-source so if you're interested go look at TrigoBot`},
+                { name: 'Repo', value: `This bot is open-source so if you're interested go look at https://github.com/TrigoBot/TrigoBot`},
         { name: 'Support', value: `If you have any problems with our bot you can join our support server: https://discord.gg/hS9q2H8fkX`},
 			)
 		await interaction.reply({embeds: [Embed] });
